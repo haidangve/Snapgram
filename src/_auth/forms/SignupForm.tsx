f13fa9e85da00f8832d33881c04beff2844123ff
haidangve <duchai.nv0909@gmail.com>
2024-03-25 16:04:09 -0600

    Update SignupForm.tsx

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -1,10 +1,26 @@
 import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
 import { Button } from "@/components/ui/button"
 
+const formSchema = z.object({
+  username: z.string().min(2).max(50),
+})
+
 const SignupForm = () => {
-  const formSchema = z.object({
-    username: z.string().min(2).max(50),
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
   })
+ 
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // This will be type-safe and validated.
+    console.log(values)
+  }
 
   return (
     <div>
