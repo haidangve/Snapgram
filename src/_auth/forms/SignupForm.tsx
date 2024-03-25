d7c29225f0a2a7af8cf2a6870f05de66b783bb62
haidangve <duchai.nv0909@gmail.com>
2024-03-25 16:25:02 -0600

    Update SignupForm.tsx

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -6,30 +6,33 @@ import {
   Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
-
-const formSchema = z.object({
-  username: z.string().min(2).max(50),
-})
+import { SignupValidation } from "@/lib/validation"
 
 const SignupForm = () => {
   // 1. Define your form.
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const form = useForm<z.infer<typeof SignupValidation>>({
+    resolver: zodResolver(SignupValidation),
     defaultValues: {
+      name: "",
       username: "",
+      email: "",
+      password: "",
     },
   })
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  function onSubmit(values: z.infer<typeof SignupValidation>) {
     // Do something with the form values.
     // This will be type-safe and validated.
     console.log(values)
   }
 
   return (
-    <div>
       <Form {...form}>
+        <div className = "sm:w-420 flex-center flex-col"> 
+
+        </div>
+
         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
           <FormField
             control={form.control}
@@ -50,7 +53,6 @@ const SignupForm = () => {
           <Button type="submit">Submit</Button>
         </form>
       </Form>
-    </div>
   )
 }
 
