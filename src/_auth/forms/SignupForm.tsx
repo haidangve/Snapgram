7d517d16604242064ef745317b842fbb328ec311
haidangve <duchai.nv0909@gmail.com>
2024-03-25 16:31:30 -0600

    Update SignupForm.tsx

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -30,7 +30,7 @@ const SignupForm = () => {
   return (
       <Form {...form}>
         <div className = "sm:w-420 flex-center flex-col"> 
-
+          <img src="/assets/images/logo.svg" alt="logo"/>
         </div>
 
         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
