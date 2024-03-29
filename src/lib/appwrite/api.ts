4d520a0f9157a9bf5ffef5261ba8db645b2aa7c0
haidangve <duchai.nv0909@gmail.com>
2024-03-29 14:45:18 -0600

    Update api.ts

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,18 +1,53 @@
-import {ID} from 'appwrite';
+import { ID } from 'appwrite';
 import { INewUser } from "@/types";
-import { account } from './config';
+import { account, appwriteConfig, avatars, databases } from './config';
 
-export async function createUserAccount(user: INewUser){
-    try{
+export async function createUserAccount(user: INewUser) {
+    try {
         const newAccount = await account.create(
             ID.unique(),
             user.email,
             user.password,
             user.name
         );
-            return newAccount;
-    }catch(error){
+
+        if (!newAccount) throw Error;
+
+        const avatarUrl = avatars.getInitials(user.name);
+
+        const newUser = await saveUserToDB({
+            accountID: newAccount.$id,
+            name: newAccount.name,
+            email: newAccount.email,
+            username: user.username,
+            imageUrl: avatarUrl,
+        });
+
+        return newAccount;
+    } catch (error) {
         console.log(error);
         return error;
+    }
+}
+
+export async function saveUserToDB(user: {
+    accountID: string;
+    email: string;
+    name: string;
+    imageUrl: URL;
+    username?: string;
+}) {
+    try {
+
+        const newUser = await databases.createDocument(
+            appwriteConfig.databaseId,
+            appwriteConfig.userCollectionId,
+            ID.unique(),
+            user
+        );
+
+        return newUser;
+    } catch (error) {
+        console.log(error);
     }
 }
